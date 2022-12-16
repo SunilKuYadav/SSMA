8c58705a5ab5b74f6410d4d863fb5b6c93aca972
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-5.local>
2022-12-16 10:27:48 +0530

    Update: initate branch

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,4 @@ import {default as Counter} from './counter';
 import {default as Dashboard} from './dashboard';
 import {default as Alarm} from './alarm';
 import {default as AddAlarm} from './alarm/AddAlarm';
-
-export {Counter, Dashboard, Alarm, AddAlarm};
+export {default as Timer} from './timer';
---
+++ src/pages/timer/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Timer = () => {
+  return <Text>Timer</Text>;
+};
+
+export default Timer;
